425c4895018cbe1a9e771dea6cfd24e3a6590fca
LakshyaG34 <lakshyagoyal34@gmail.com>
2023-08-06 19:29:21 +0530

    Initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Navbar, Main, Product, Footer } from "../Components";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Product />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
---
+++ src/Pages/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Navbar, Product } from "../Components"
+
+
+const Products = () => {
+  return (
+    <>
+      <Navbar />
+      <Product />
+      <Footer />
+    </>
+  )
+}
+
+export default Products
